a94100b84cd7777069105c1613b927e00499281c
Feli <maik@ehemanns.de>
2026-01-23 01:32:56 +0100

    Removed unused class

--- src/main/java/de/feli490/utils/core/sql/SQLRunnable.java
+++
@@ -1,8 +0,0 @@
-package de.feli490.utils.core.sql;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface SQLRunnable<T> {
-    public T execute(Connection connection, SQLConnection sqlConnection) throws SQLException;
-}
